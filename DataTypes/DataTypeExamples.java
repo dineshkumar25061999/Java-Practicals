cd5b2a1035c25790458e4001b8a4298ba7ca3a9a
dineshkumar25061999 <119724074+dineshkumar25061999@users.noreply.github.com>
2022-12-03 12:28:27 +0530

    Add files via upload

---
+++ DataTypes/DataTypeExamples.java
@@ -0,0 +1,37 @@
+package basic;
+
+public class DataTypeExamples {
+
+	public static void main(String[] args) {
+
+		byte b=127;
+		
+		short s=-150;
+		
+		int i=200000000;
+		
+		long l = 944424544;
+		
+		float f=298.6154f;
+		
+		double d=1544.544447784464;
+		
+		char c = 'z';
+		
+		char ch= 98;
+		 
+		boolean r=true;
+		
+		System.out.println(" byte "+b);
+		
+		System.out.println(" short "+s);
+		
+		System.out.println(" int "+i);
+		System.out.println(" long "+l);
+		System.out.println(" float "+f);
+		System.out.println(" double "+d);
+		System.out.println(" char "+c+" , "+ch);
+		System.out.println(" boolean "+r);
+	}
+
+}
